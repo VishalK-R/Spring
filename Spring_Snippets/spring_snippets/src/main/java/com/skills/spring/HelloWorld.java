aaf47ed6a68394475c2062e77119896840232e7c
VishalK-R <bruhath.vishal@gmail.com>
2025-04-10 16:56:13 +0530

    Configuring Spring Without Using Spring Boot

---
+++ Spring_Snippets/spring_snippets/src/main/java/com/skills/spring/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.skills.spring;
+
+public class HelloWorld {
+	private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Message: " + message);
+    }
+}
