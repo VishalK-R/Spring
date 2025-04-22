9732168bb58cf3b51e8b5a1ad7717c6b28e98264
Vishal-KR31 <bruhath.vishal@outlook.com>
2025-04-22 18:13:35 +0530

    commit-5 Autowiring

--- Spring_Snippets/spring_snippets/src/main/java/com/skills/spring/App.java
+++ Spring_Snippets/spring_snippets/src/main/java/com/skills/spring/App.java
@@ -3,6 +3,8 @@ package com.skills.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.skills.meal.Meal;
+
 
 /**
  * Hello world!
@@ -12,18 +14,18 @@ public class App {
 	public static void main(String[] args) {
 		// Load the Spring configuration file
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		Vegetable vegetableUsingPNameSpace = (Vegetable) context.getBean("vegetableUsingPNameSpace");
-		com.skills.meal.Fruit fruitWithNameBean = (com.skills.meal.Fruit) context.getBean("fruitWithNameBean");
-		com.skills.meal.Vegetable vegetableWithNameBean = (com.skills.meal.Vegetable) context.getBean("vegetableWithNameBean");
-		com.skills.meal.Meal mealOuterBean = (com.skills.meal.Meal) context.getBean("mealOuterBean");
-		com.skills.meal.Fruit fruitWithInitAndDestroy = (com.skills.meal.Fruit) context.getBean("fruitWithInitAndDestroy");
 		
-		System.out.println(vegetableUsingPNameSpace.talkAboutYourself());
-		System.out.println(fruitWithNameBean.talkAboutYourself());
-		System.out.println(vegetableWithNameBean.talkAboutYourself());
-		System.out.println(mealOuterBean.talkAboutYourSelf());
-		System.out.println(fruitWithInitAndDestroy.talkAboutYourself());
+//		 Meal mealbeanAutowiredByType = (Meal)context.getBean("mealbeanAutowiredByType");
+//		 System.out.println(mealbeanAutowiredByType.talkAboutYourSelf());
+		 
+//		Meal mealbeanAutowiredByName = (Meal)context.getBean("mealbeanAutowiredByName");
+//		System.out.println(mealbeanAutowiredByName.talkAboutYourSelf());
+		
+//		Meal mealbeanAutowiredByConstructor = (Meal)context.getBean("mealbeanAutowiredByConstructor");
+//		System.out.println(mealbeanAutowiredByConstructor.talkAboutYourSelf());
+		
+		Meal mealbeanAutowiredByDefault = (Meal)context.getBean("mealbeanAutowiredByDefault");
+		System.out.println(mealbeanAutowiredByDefault.talkAboutYourSelf());
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
